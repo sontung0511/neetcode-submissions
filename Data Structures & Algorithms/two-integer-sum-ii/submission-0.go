f6819b7857656b3324cad640803459ef22b8d1ef
Lucas <92152067+sontung0511@users.noreply.github.com>
2026-07-09 22:11:05 +0700

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.go
@@ -0,0 +1,16 @@
+func twoSum(numbers []int, target int) []int {
+	left,right := 0,len(numbers)-1
+	for left < right {
+		sum := numbers[left] + numbers[right]
+		if sum == target {
+			return []int{left+1,right+1}
+		}
+		if sum < target {
+			left++
+		}
+		if sum > target {
+			right--
+		}
+	}
+	return nil
+}
